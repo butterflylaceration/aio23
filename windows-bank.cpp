97bea07639ead344d76fd9e11502ba114c3b82a9
Akira Li <abillionpaperflowers@gmail.com>
2023-08-31 11:57:18 +1000

    Update windows-bank.cpp

--- windows-bank.cpp
+++ windows-bank.cpp
@@ -1,3 +1,19 @@
+/*
+QUESTION:
+You are a painter retiring in N days time and would like to plan the rest of your career. You currently
+have an artistic skill of s = 1, but can attend free art classes to increase it.
+There are two types of days, represented by an uppercase character:
+• C: There is an art class today. You can choose to attend the art class and increase your skill s by
+1, or you can spend the day painting, earning s dollars.
+• M: There is no art class today. You must spend the day painting, earning s dollars.
+What is the most money (in dollars) you can retire with? You are guaranteed that this number will not
+exceed 2 000 000 000.
+Input
+• The first line of input contains the integer N.
+• The second line of input contains a string of N characters, describing the days.
+Output
+Your program must output the most money (in dollars) you can retire with.
+*/
 
 
 #include <bits/stdc++.h>
